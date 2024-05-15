f086f53ac62e1bb9e777ec836c79ee0368b3b9c1
nibnualdi <nibnualdi@gmail.com>
2024-05-15 07:14:14 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+console.log("Wait a moment!");
+
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
